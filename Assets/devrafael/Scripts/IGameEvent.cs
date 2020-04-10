783453c9f78fd1fc5acb8f4b8e6405225bbddfd3
Rafael T. Ribeiro <rafaeltrpessoal@gmail.com>
2020-04-10 18:57:41 -0300

    Update folder architecture

---
+++ Assets/devrafael/Scripts/IGameEvent.cs
@@ -0,0 +1,18 @@
+public interface IGameEvent { }
+
+public class WPressEvent : IGameEvent
+{
+
+}
+
+public class CubeSpawnEvent : IGameEvent
+{
+    public float spawnTime = 0;
+    public UnityEngine.Vector3 spawnPos = UnityEngine.Vector3.zero;
+
+    public CubeSpawnEvent(float time, UnityEngine.Vector3 pos)
+    {
+        spawnTime = time;
+        spawnPos = pos;
+    }
+}
